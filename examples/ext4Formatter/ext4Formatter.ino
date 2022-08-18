17855dbfae3f20788ae671178bb4f7d415a6d4a8
Warren Watson <wwatson4506@gmail.com>
2022-08-18 16:04:45 -0700

    Add files via upload

--- examples/ext4Formatter/ext4Formatter.ino
+++ examples/ext4Formatter/ext4Formatter.ino
@@ -2,13 +2,33 @@
 
 #include "ext4FS.h"
 
-extern USBHost myusb;
+USBHost myusb;
 
-LWextFS myext4fs;
-LWextFS myext4fs1;
-LWextFS myext4fs2;
-LWextFS myext4fs3;
-LWextFS myext4fs4;
+// Setup USBHost_t36 and as many HUB ports as needed.
+USBHub hub5(myusb);
+USBHub hub6(myusb);
+USBHub hub7(myusb);
+USBHub hub8(myusb);
+
+// Instances for the number of USB drives you are using.
+USBDrive extDrive1(myusb);
+USBDrive extDrive2(myusb);
+USBDrive extDrive3(myusb);
+
+USBDrive *drive_list[] = {&extDrive1, &extDrive2, &extDrive3};
+
+// EXT usage with an SDIO card.
+#define SD_CONFIG SdioConfig(FIFO_SDIO)
+SdCardFactory cardFactory;
+SdCard *sd = cardFactory.newCard(SD_CONFIG);
+
+//**********************************************************************
+// Setup four instances of LWextFS (four mountable partittions).
+//**********************************************************************
+ext4FS myext4fs1;
+ext4FS myext4fs2;
+ext4FS myext4fs3;
+ext4FS myext4fs4;
 
 #define sda1 0	// First partition on first USB device.
 #define sda2 1	// Second partition on first USB device.
@@ -17,10 +37,10 @@ LWextFS myext4fs4;
 #define sdd1 12  // First partition on SD device
 
 // Set this to one of the above devices.
-#define sdxx sdd1
+#define sdxx sda1
 
 // Grab a pointer to the mount list.
-bd_mounts_t *ml = get_mount_list();
+bd_mounts_t *ml = myext4fs1.get_mount_list();
 
 void setup() {
   
@@ -37,13 +57,30 @@ void setup() {
 //ext4_dmask_set(DEBUG_ALL);
 
   Serial.printf("%cTeensy ext4 formatter\n\n",12);
-  Serial.println("Initializing LWextFS ...");
+  Serial.println("Initializing ext4FS ...");
+  Serial.println("Initializing all availble lwext4 devices.\n");
+  Serial.println("Please Wait...\n");
 
   myusb.begin();
+  delay(3000);
+  myusb.Task();
 
-  Serial.println("Initializing device sdxx.\n");
-  myext4fs.init_block_device(sdxx);  
-  
+  for (uint16_t drive_index = 0; drive_index < (sizeof(drive_list)/sizeof(drive_list[0])); drive_index++) {
+    USBDrive *pdrive = drive_list[drive_index];
+    if (*pdrive) {
+      if (!pdrive->filesystemsStarted()) {
+        pdrive->startFilesystems();
+      }
+      myext4fs1.init_block_device(pdrive, drive_index);  
+    }
+  }
+  // Init SD card (Block device 3) fixed.
+  if(myext4fs1.init_block_device(sd, 3) == EOK) {  
+    Serial.printf("SD card is inserted...\n");
+  } else {
+    Serial.printf("SD card is NOT inserted...\n");
+  }
+
   Serial.print("\nWARNING: This will destroy any data on ---> ");
   Serial.println(mpName[sdxx]);
   waitforInput();
