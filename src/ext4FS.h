2abd64206d2483a419a9af093ae0dc6cfe16ccee
Warren Watson <wwatson4506@gmail.com>
2023-12-08 05:33:42 -0800

    Update ext4FS.h

--- src/ext4FS.h
+++ src/ext4FS.h
@@ -472,7 +472,7 @@ public:
 	int lwext_init_devices(void);
 	bool scan_mbr(uint8_t dev);
 	int lwext_mount(uint8_t dev);
-	bool lwext_umount(uint8_t dev);
+	int lwext_umount(uint8_t dev);
 	int getMountStats(const char * vol, struct ext4_mount_stats *mpInfo);
 	int lwext_mkfs (struct ext4_blockdev *bdev, const char *label = "");
 protected:
